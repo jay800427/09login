12f12982dd8521387c30f90ec6ee115d6d9eb440
jay800427 <jay8000427@gmail.com>
2019-10-31 10:49:40 +0800

    登入並連結一份資料表

--- member_center.php
+++ member_center.php
@@ -8,6 +8,17 @@
   <title>會員中心</title>
   <link rel="stylesheet" href="style.css">
 </head>
+<style>
+  table{
+    border-collapse:collapse;
+    border-spacing:0;
+  }
+  td{
+    border:1px solid #ccc;
+    padding:10px;
+    text-align:center;
+  }
+  </style>
 <body>
   <div class="member">
     <div class="wellcome">
@@ -15,7 +26,50 @@
     </div>
     <div class="private">
       <!--請自行設計個人資料的呈現方式並從資料庫取得會員資料-->
+      <?php
+      $dsn="mysql:host=localhost;charset=utf8;dbname=mydb"; //連結資料庫
+      $pdo=new PDO($dsn,'root','');
 
+      $sql="select * from user where id='".$_GET['id']."'";
+      //echo $sql;
+      $user=$pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+      //print_r($user);
+
+      ?>
+      <table>
+        <tr>
+          <td>id</td>
+          <td><?=$user['id'];?></td>
+        </tr>
+        <tr>
+          <td>acc</td>
+          <td><?=$user['acc'];?></td>
+        </tr>
+        <tr>
+          <td>pw</td>
+          <td><?=$user['pw'];?></td>
+        </tr>
+        <tr>
+          <td>name</td>
+          <td><?=$user['name'];?></td>
+        </tr>
+        <tr>
+          <td>addr</td>
+          <td><?=$user['addr'];?></td>
+        </tr>
+        <tr>
+          <td>tel</td>
+          <td><?=$user['tel'];?></td>
+        </tr>
+        <tr>
+          <td>birthday</td>
+          <td><?=$user['birthday'];?></td>
+        </tr>
+        <tr>
+          <td>email</td>
+          <td><?=$user['email'];?></td>
+        </tr>
+      </table>
 
 
 
